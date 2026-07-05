b89de16f5c9749faf201b36e1186f50e97aaec9e
Yuki Kikuchi <andres8.yuuki@gmail.com>
2026-07-05 22:51:24 +0900

    Revert "Feature/demo data"

--- frontend/src/routes/(app)/chat/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-	import ChatPanel from '$lib/components/ChatPanel.svelte';
-</script>
-
-<ChatPanel />
--- frontend/src/routes/(app)/graph/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-	import GraphView from '$lib/components/GraphView.svelte';
-</script>
-
-<div class="h-[calc(100vh-4rem)]"><GraphView /></div>
